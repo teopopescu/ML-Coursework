d1dac84206b25d35de75048c39ba8b609227407a
PG-Popescu <Teodor.Popescu@city.ac.uk>
2018-11-20 14:53:58 +0000

    random forest code from scratch

--- random_forest_model_selection.m
+++ random_forest_model_selection.m
@@ -1,56 +1,91 @@
     
+
 %% Read the numerical train dataset for Random Forest Model Selection
+%% Clear everything
+clear all
+close all
 
-%read the data
+%read the numerical dataset for Random Forest Model selection
 train_rf=readtable('training_num80.csv');
+
+%% specify that the last column is ordinal categorical data
+avalues={'unacc','acc','good','vgood'};
+train_rf.acceptability=categorical(train_rf.acceptability,avalues,'Ordinal',true);
+
+
+% do Random Forest yourself; 
+
+%build all models in a nested for loop iterating through the hyperparameters;( nb of trees, depth )
+%Mdl = TreeBagger....
+%cross-validation 
+%implement treeBagger after ON THE BEST MODEL taken from cross-validation;
+%generate table with hyperparameters and out-of-bag error for each model
+
+%setting a list of hyperparameters
+nb_of_trees=[10 20 40 50];
+max_depth=[10 5 15];
+nb_of_splits=[100 200 50];
+variables_at_random=[2 3 4 5];
+
+rf_initialize_hyperparams;
+for trees = nb_of_trees
+    for depth = max_depth
+        for splits = nb_of_splits
+            for at_random = variables_at_random
+%split predictors and target variables
+        features=train_rf(:,1:6);
+        labels=train_rf(:,7);
+%set up the optimization function
+    Mdl = TreeBagger(trees, features,labels,...
+    'Method','classification',...
+    'ClassNames', categorical({'acc'; 'good'; 'unacc'; 'vgood'}),...
+    'MaxNumSplits', splits, ...
+    'NumPredictorsToSample', at_random, ...  
+    'OOBPrediction','on');
+
+%No cross-validation needed as random forest chooses a bagged subset of input data at random
+% setting NumPredictorsToSample to any value but 'all' enables TreeBagger to behave like RandomForest     
+     %predict, store OOB error for each model
+     
+     
+     %save the results into one table
+     number_of_trees=[number_of_trees;trees];
+     maximum_depth =[maximum_depth;depth];
+     number_of_splits=[number_of_splits;splits];
+     variables_to_sample = [ variables_to_sample;at_random];
+     out_of_bag_error=[out_of_bag_error;oobError(Mdl)];
+
+            end
+        end
+    end
+    
+end
+
+% Get the model with the best accuracy from all models
+
+%Take the best model and predict on test set; 
+
+%read the numerical dataset for Random Forest Model selection
 test_rf=readtable('test_num80.csv');
-
-%Train
-[trainedClassifier, validationAccuracy] = trainRandomForestClassifier(train_rf)
-
-
-%Feature importance
-%https://www.kaggle.com/niklasdonges/end-to-end-project-with-python
-%https://towardsdatascience.com/random-forest-in-python-24d0893d51c0
-
-% Cross-validaton
 
 
 % Predict Results on the test set
 test_features = table2array(test_nb(:,1:6));
 test_labels = test_nb(:,7);
-     
-predicted_labels = trainedClassifier.predictFcn(test_rf) % <-- i don't think this is right. 
 
-%calculate cross-entropy for the model on train set;
+%Feature importance
+%NO ! Principal Component Analysis (NB used corrcoef for analysis); <-- done on the trainedRandomForest;
+% 2 models; one with all and one with 3-4 parameters; 
+%have an optimized model 
+%NO NEED for crossv-valid sa it's done on the for loop
 
-%Hyperparameter tuning: that means “adjust the settings to improve performance” (The settings are known as hyperparameters 
-%to distinguish them from model parameters learned during training). The most common way to do this 
-%is simply make a bunch of models with different settings, 
-%evaluate them all on the same validation set, and see which one does best.
-
-% Hyperparameter tuning; 
-    %Grid search for hyperparameter optimization;also random search or bayesian optimization; 
-
-%- criterion: The function to measure the quality of a split. Supported criteria are “gini” for the Gini impurity and “entropy” for the information gain.
-%min_samples_leaf: The minimum number of samples required to be at a leaf node. A split point at any depth will only be considered if it leaves at least min_samples_leaf training samples in each of the left and right branches.
-%min_samples_split 
-%and n_estimators.
-
-% display; number of trees in the forest; one for number of features to sample at random in each node; 
- 
-%Change the number of trees in the forest
-%Change the maximum depth of the trees
-%Change the size of the forest
 
 % Save the results against their targets in a file
 predictions= [test_labels array2table(predicted_labels)];
-VarNames={'target','predictions'};
-predictions.Properties.VariableNames = VarNames;
 writetable(predictions,'final_results_random_forest.csv');
 
 %Further Evaluation
-    %Cross entropy and accuracy; 
+    %accuracy; 
     %Confusion Matrix
     %Precision and Recall
     %F-Score
@@ -61,20 +96,3 @@ writetable(predictions,'final_results_random_forest.csv');
 %delete training and test files after process is complete 
 
 
-%Links
-%https://towardsdatascience.com/demystifying-hyper-parameter-tuning-acb83af0258f
-%https://www.kaggle.com/niklasdonges/end-to-end-project-with-python
-%https://towardsdatascience.com/metrics-to-evaluate-your-machine-learning-algorithm-f10ba6e38234
-%https://towardsdatascience.com/understanding-hyperparameters-and-its-optimisation-techniques-f0debba07568
-%https://towardsdatascience.com/cross-validation-70289113a072
-%https://towardsdatascience.com/cross-validation-in-machine-learning-72924a69872f
-%https://towardsdatascience.com/demystifying-cross-entropy-e80e3ad54a8
-%https://towardsdatascience.com/demystifying-entropy-f2c3221e2550
-%https://stats.stackexchange.com/questions/344220/how-to-tune-hyperparameters-in-a-random-forest
-%https://www.analyticsvidhya.com/blog/2015/06/tuning-random-forest-model/
-%https://uk.mathworks.com/matlabcentral/answers/321200-random-forest-using-classification-learner-app
-%https://towardsdatascience.com/metrics-to-evaluate-your-machine-learning-algorithm-f10ba6e38234
-%https://www.mathworks.com/matlabcentral/answers/151699-am-i-computing-cross-entropy-incorrectly
-%https://www.jeremyjordan.me/hyperparameter-tuning/
-%https://towardsdatascience.com/understanding-hyperparameters-and-its-optimisation-techniques-f0debba07568
-%https://towardsdatascience.com/random-forest-in-python-24d0893d51c0
